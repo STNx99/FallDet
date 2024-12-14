fb04190f886abbc0eb4ca6586b4feedd9a0b1ebc
STNx99 <www.thongvo123hanaver@gmail.com>
2024-12-14 19:11:53 +0700

    update point

--- main.py
+++ main.py
@@ -1,57 +1,39 @@
 import cv2
-import cvzone
-import math
 from ultralytics import YOLO
 
-cap = cv2.VideoCapture("teamtest.mp4")
+model = YOLO('yolov8m-pose.pt')
 
-model = YOLO('yolov8x.pt')
+video_path = 'gymtest.mp4'
+cap = cv2.VideoCapture(video_path)
 
-classnames = []
-with open('classnames.txt', 'r') as f:
-    classnames = f.read().splitlines()
-
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (680, 382))
+    if not ret:
+        break
 
-    results = model(frame)
+    results = model.predict(source=frame, save=False, save_txt=False)
 
-    for info in results:
-        parameters = info.boxes
-        for box in parameters:
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            confidence = box.conf[0]
-            class_detect = box.cls[0]
-            class_detect = int(class_detect)
-            class_detect = classnames[class_detect]
-            conf = math.ceil(confidence * 100)
+    for result in results:
+        for pose in result.keypoints:
+            keypoints = pose.xy[0]
 
-            # implement fall detection using the coordinates x1,y1,x2
-            height = y2 - y1
-            width = x2 - x1
-            threshold = height - width
+            if keypoints.shape[0] >= 13:
+                nose_x, nose_y = keypoints[0][:2]
+                left_hip_x, left_hip_y = keypoints[11][:2]
+                right_hip_x, right_hip_y = keypoints[12][:2]
 
-            if conf > 80 and class_detect == 'person':
-                cvzone.cornerRect(frame, [x1, y1, width, height], l=30, rt=6)
-                cvzone.putTextRect(frame, f'{class_detect}', [x1 + 8, y1 - 12], thickness=2, scale=2)
-
-            if threshold < 0:
-                cvzone.putTextRect(frame, 'Fall Detected', [height, width], thickness=2, scale=2)
-
+                print(f"Nose: x={nose_x.item():.2f}, y={nose_y.item():.2f}")
+                print(f"Left Hip: x={left_hip_x.item():.2f}, y={left_hip_y.item():.2f}")
+                print(f"Right Hip: x={right_hip_x.item():.2f}, y={right_hip_y.item():.2f}")
             else:
-                pass
+                print("Not enough keypoints detected.")
 
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('t'):
+    processed_frame = results[0].plot()
+
+    cv2.imshow('YOLOv8 Pose Estimation', processed_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cap.release()
 cv2.destroyAllWindows()
-
-# from ultralytics import YOLO
-#
-# model = YOLO("yolov8x-pose.pt")
-#
-# result = model(source="gymtest", show = True, conf = 0.9, stream = True)
